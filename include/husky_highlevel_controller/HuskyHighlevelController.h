2ee73d1d24f02ce57a1edb9f4be2c9107f36515c
Esraa-Magdy-Mahmoud <esraaelelimy@gmail.com>
2018-01-20 11:29:29 +0200

    creating HuskyHighlevelController class

---
+++ include/husky_highlevel_controller/HuskyHighlevelController.h
@@ -0,0 +1,34 @@
+#ifndef HUSKYHIGHLEVELCONTROLLER_H_
+#define HUSKYHIGHLEVELCONTROLLER_H_
+
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+namespace husky_highlevel_controller
+{
+	class HuskyHighlevelController
+	{
+		public:
+			// constructor
+			HuskyHighlevelController(ros::NodeHandle& nodeHandle);
+			// destructor
+			virtual ~HuskyHighlevelController();
+		private:
+			// Read ros param, return true if successful
+			bool readParameters();
+			// Callback function
+			void scanCallback(const sensor_msgs::LaserScan &msg);
+
+			ros::NodeHandle& nodeHandle_;
+			ros::Subscriber  scan_sub_;
+			std::string subscriberTopic_;
+			int queue_size;
+			 
+	};
+	
+}
+
+
+
+
+#endif
