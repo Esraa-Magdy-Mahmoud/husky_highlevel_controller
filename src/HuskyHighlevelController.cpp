7eef4ffca789f347ba6d322dbe927469ef643733
Esraa-Magdy-Mahmoud <esraaelelimy@gmail.com>
2018-01-20 11:29:51 +0200

    creating HuskyHighlevelController class

---
+++ src/HuskyHighlevelController.cpp
@@ -0,0 +1,38 @@
+#include "husky_highlevel_controller/HuskyHighlevelController.h"
+
+
+
+namespace husky_highlevel_controller
+{
+   HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) :
+    nodeHandle_(nodeHandle)
+    {
+        if (!readParameters()) 
+        {
+            ROS_ERROR("Could not read parameters.");
+            ros::requestShutdown();
+        }
+        scan_sub_ = nodeHandle_.subscribe(subscriberTopic_, queue_size , &HuskyHighlevelController::scanCallback, this);
+        ROS_INFO("Successfully launched node.");
+    }
+
+    HuskyHighlevelController::~HuskyHighlevelController()
+    {
+    }
+    
+    bool HuskyHighlevelController::readParameters()
+    {
+        if (!nodeHandle_.getParam("scan_sub_topic", subscriberTopic_)) return false;
+       
+        if (!nodeHandle_.getParam("scan_sub_queue_size", queue_size))return false;
+       
+        return true;
+    }
+    void HuskyHighlevelController::scanCallback(const sensor_msgs::LaserScan &msg)
+    {
+
+    }
+
+}
+
+
