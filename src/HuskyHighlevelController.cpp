2752f3a70e997786aacd9631d22075971ecdd4ad
Esraa-Magdy-Mahmoud <esraaelelimy@gmail.com>
2018-01-21 13:18:00 +0200

    Added p controller method

--- src/HuskyHighlevelController.cpp
+++ src/HuskyHighlevelController.cpp
@@ -39,6 +39,9 @@ namespace husky_highlevel_controller
     void HuskyHighlevelController::scanCallback(const sensor_msgs::LaserScan &scan_msg)
     {
         float smallest_distance = 9999;
+        // the angle corresponding to the minimum distance
+      
+
         //number of the elements in ranges array
         int arr_size = floor((scan_msg.angle_max-scan_msg.angle_min)/scan_msg.angle_increment);
         for (int i=0 ; i< arr_size ;i++)
@@ -46,11 +49,47 @@ namespace husky_highlevel_controller
             if (scan_msg.ranges[i] < smallest_distance)
             {
                 smallest_distance = scan_msg.ranges[i];
+                alpha_pillar = (scan_msg.angle_min + i*scan_msg.angle_increment);
             }
         }
-        ROS_INFO_STREAM("the smallest distance measurement from the laser scanner (m): "<<smallest_distance<<'\n');
+
+        //Pillar position 
+        x_pillar = smallest_distance*cos(alpha_pillar);
+        y_pillar =  smallest_distance*sin(alpha_pillar);
+
+        //ROS_INFO_STREAM("the smallest distance measurement from the laser scanner (m): "<<smallest_distance<<'\n');
+        //ROS_INFO_STREAM("pillar alpha (m): "<<alpha_pillar<<'\n');
+        
+        //P-Controller to drive husky towards the pillar
+        HuskyHighlevelController::pController();
+        cmd_pub_.publish(vel_msg_);
+        //ROS_INFO_STREAM("vel msg (m): "<<vel_msg_.linear.x <<"angulr zft "<< vel_msg_.angular.z<<'\n');
 
 
+
+    }
+    void HuskyHighlevelController::pController()
+    {
+        //propotinal gain
+        float p_gain_vel = 0.2;
+        float p_gain_ang = 10;
+        if (x_pillar <= 0.5 )
+        {
+             vel_msg_.linear.x = 0;
+             vel_msg_.linear.y = 0; 
+             vel_msg_.angular.z =0;
+
+        }
+        else 
+        {
+             vel_msg_.linear.x = x_pillar * p_gain_vel  ;
+             vel_msg_.linear.y = y_pillar * p_gain_vel ; 
+             vel_msg_.angular.z = -alpha_pillar ;
+
+        }
+        
+       
+       
     }
 
 }
