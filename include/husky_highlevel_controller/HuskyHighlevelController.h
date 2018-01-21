d78731584b4381cf97c742002375a3979fca8b97
Esraa-Magdy-Mahmoud <esraaelelimy@gmail.com>
2018-01-21 13:17:24 +0200

    Added COntroller method

--- include/husky_highlevel_controller/HuskyHighlevelController.h
+++ include/husky_highlevel_controller/HuskyHighlevelController.h
@@ -17,18 +17,29 @@ namespace husky_highlevel_controller
 		private:
 			// Read ros param, return true if successful
 			bool readParameters();
-			// Callback function
+			// Callback method
 			void scanCallback(const sensor_msgs::LaserScan &scan_msg);
+
+			//P-controller method
+			void pController();
 
 			// node habdle
 			ros::NodeHandle& nodeHandle_;
 			// subscriber to /scan topic
 			ros::Subscriber  scan_sub_;
 			//publisher to /cmd_vel
-			ros::Publisher   cmd_pub_;
+			ros::Publisher  cmd_pub_;
 			
 			//twist msg
-			geometry_msgs::Twist vel_msg;
+			geometry_msgs::Twist vel_msg_;
+
+			//pillar position
+			float x_pillar;
+			float y_pillar;
+			// the angle of the pillar with respect to the x_axis
+			float alpha_pillar;
+
+ 
 
 			std::string subscriberTopic_;
 			int queue_size;
